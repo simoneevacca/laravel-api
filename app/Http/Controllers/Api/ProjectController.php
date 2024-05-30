d9715b729f7d49b05e1f571d0b292643d1989a71
simoneevacca <v.simone.vacca@gmail.com>
2024-05-30 14:36:14 +0200

    add Api/ProjectController with method to get json of projects

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::paginate(3);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+}
